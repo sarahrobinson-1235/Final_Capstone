e51cf76af7fd33c04d7e3157cad71afec1faf741
sarah <sarahaleesia1@gmail.com>
2021-02-26 14:02:30 -0600

    updates user routes/controllers/views

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,48 @@
 class Api::UsersController < ApplicationController
+  # before_action :authenticate_user
+  
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+  def show
+    
+    id = params["id"]
+    @user = User.find_by(id: id)
+    
+    if @user
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name] || @user.name
+    @user.email = params[:email] || @user.email
+    @user.image_url = params[:image_url] || @user.image_url
+    if @user.save
+      render "_users.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: { message: "Profile Deleted!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/users" => "users#create"
+    post "/users" => "users#create"
+    get "/users/:id" => "users#show"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+
+    post "/sessions" => "sessions#create"
+
   end
 end
