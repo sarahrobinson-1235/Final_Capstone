c53aba005b3c84ccd06de9e7e834e80ce1cb4812
sarah <sarahaleesia1@gmail.com>
2021-03-18 01:58:39 -0500

    adds web socket

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -1,7 +1,8 @@
 class Api::QuotesController < ApplicationController
 
   def daily
-    @quote = HTTP.get("https://zenquotes.io/api/today").body
+    @quote = HTTP.get("https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium").parse
     render "show.json.jb"
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_many :posts, dependent: :destroy
   has_many :boards, dependent: :destroy
-
+  has_many :messages
 end
